f3dee94f1e67d2e2ddd994ca7c90b9f61ca0dcab
Azhar Toor <azhartoor07@gmail.com>
2026-04-11 20:15:30 +0500

    Reverted the settings change

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bo%o%^wmtpm2kfvf0l!ao+ed#mcem%n(ya+#=wwq@uuiepvrq&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
